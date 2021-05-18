2bbcb6cea8846bde560fc06012aa517cf1d3c034
Arnaud Levy <contact@arnaudlevy.com>
2021-05-18 12:57:38 +0200

    init

---
+++ lib/kamifusen.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kamifusen/version"
+
+module Kamifusen
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kamifusen/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Kamifusen
+  VERSION = "0.1.0"
+end

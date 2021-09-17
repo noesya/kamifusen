c334a4370f2a5a3db252f092a4c5163d643f19cf
Arnaud Levy <contact@arnaudlevy.com>
2021-09-17 16:13:42 +0200

    1.9 breakpoints

--- lib/kamifusen.rb
+++ lib/kamifusen.rb
@@ -9,6 +9,36 @@ module Kamifusen
   mattr_accessor :with_webp
   @@with_webp = true
 
+  mattr_accessor :sizes
+  @@sizes = [
+    # 360, # Old android
+    375, # Old iPhone
+    # 414, # ?
+    576, # Tablets desktop
+    640, # iPhone SE, some tablets
+    750, # iPhone 6/7/8, 375@2x
+    768, # Old iPads, Old desktops
+    # 828, # ?
+    # 992, # Breakpoint bootstrap
+    1080, # iPhone 6/7/8 plus, 414@2.608 (sorry)
+    # 1125, # iPhone 10, 375@3x
+    1152,
+    # 1172, # iPhone 12, 390@3x
+    1200, # Desktop
+    1366, # Desktop
+    # 1400, # Breakpoint boostrap
+    1440, # Samsung Galaxy S20, 360@4x
+    1536, # Desktop, some iPads
+    1920, # Desktop 2k
+    2048, # Some iPad
+    2240, # Desktop iMac M1 chipset
+    2880, # Desktop MacBook Pro/Air 13" @2x
+    3072  # Desktop MacBook Pro 16" @2x
+  ]
+
+  mattr_accessor :quality
+  @@quality = 80
+
   class Engine < ::Rails::Engine
   end
 
--- lib/kamifusen/version.rb
+++ lib/kamifusen/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Kamifusen
-  VERSION = "1.8"
+  VERSION = "1.9"
 end

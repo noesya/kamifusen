bc2ecfaf9ba42d3b3be2a9b7f7118df7208f43f1
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-11-17 18:11:41 +0100

    clean notyf

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -2,37 +2,4 @@
 //= require bootstrap-sprockets
 //= require @rails/ujs
 //= require notyf/notyf.min
-
-Rails.start();
-
-window.addEventListener('DOMContentLoaded', function () {
-    'use strict';
-    var notyfAlert = document.querySelector('.js-notyf-alert'),
-        notyfNotice = document.querySelector('.js-notyf-notice'),
-        notyf = new Notyf(),
-        openNotyf;
-
-    openNotyf = function (element, type, backgroundColor) {
-        notyf.open({
-            type: type,
-            icon: false,
-            background: backgroundColor,
-            position: {
-                x: 'center',
-                y: 'bottom'
-            },
-            message: element.innerText,
-            duration: 9000,
-            ripple: true,
-            dismissible: true
-        });
-    };
-
-    if (notyfAlert !== null) {
-        openNotyf(notyfAlert, 'error', '#E1531B');
-    }
-
-    if (notyfNotice !== null) {
-        openNotyf(notyfNotice, 'success', '#0D3CC8');
-    }
-});
+//= require_tree ./application
